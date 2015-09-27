a149df5096dadd4cba1961c71fc0f83b182c82a4
Andrii Kovalchuk <andrii@diversido-mobile.com>
2015-09-27 22:06:40 +0300

    Adjusted TimeMatchingService to allow cross day rules.

--- src/main/scala/congestioncharge/services/TimeMatchingService.scala
+++ src/main/scala/congestioncharge/services/TimeMatchingService.scala
@@ -2,23 +2,37 @@ package congestioncharge.services
 
 import congestioncharge.entities._
 import congestioncharge.utils.IntervalImplicits._
+import congestioncharge.utils.LocalTimeInterval
 import org.joda.time._
 
 class TimeMatchingService {
 
   def getTotalTimeMatchingPolicyRule(trackedTime: Interval, rule: ReceiptPolicyRule): Period = {
-    trackedTime
-      .splitByDays()
-      .filter { i => rule.weekDays.contains(i.getStart().getDayOfWeek()) }
-      .foldLeft (Period.ZERO) { (p, i) => {
-        val ruleStart = i.getStart().toLocalDate().toDateTime(rule.timeInterval.from)
-        val ruleEnd = i.getStart().toLocalDate().toDateTime(rule.timeInterval.to)
+    (for {
+      i <- trackedTime.splitByDays().filter { i => rule.weekDays.contains(i.getStart().getDayOfWeek()) }
+      r <- splitIfCrossDay(rule)
+    } yield (i, r))
+      .foldLeft (Period.ZERO) { case (p, (i, r)) => {
+        val ruleStart = i.getStart().toLocalDate().toDateTime(r.timeInterval.from)
+        var ruleEnd = i.getStart().toLocalDate().toDateTime(r.timeInterval.to)
+        if(r.timeInterval.to == LocalTime.MIDNIGHT)
+          ruleEnd = ruleEnd.plusDays(1)
+
         val ruleInterval = new Interval(ruleStart, ruleEnd)
         if (ruleInterval.overlaps(i))
           p.plusMillis(ruleInterval.overlap(i).toDurationMillis().toInt)
         else
           p
     }}
+  }
+
+  private def splitIfCrossDay(rule: ReceiptPolicyRule) = {
+    if(rule.timeInterval.from.getMillisOfDay() <= rule.timeInterval.to.getMillisOfDay())
+      List(rule)
+    else
+      List(
+        ReceiptPolicyRule(rule.name, rule.pricePerHour, rule.weekDays, LocalTimeInterval(LocalTime.MIDNIGHT, rule.timeInterval.to)),
+        ReceiptPolicyRule(rule.name, rule.pricePerHour, rule.weekDays, LocalTimeInterval(rule.timeInterval.from, LocalTime.MIDNIGHT)))
   }
 
 }
--- src/test/scala/unit/TimeMatchingServiceTest.scala
+++ src/test/scala/unit/TimeMatchingServiceTest.scala
@@ -11,6 +11,7 @@ class TimeMatchingServiceTest extends FlatSpec with Matchers with Assertions {
 
   private val _amRule = ReceiptPolicyRule("AM rate", 2, WeekDays.WorkWeek, LocalTimeInterval(new LocalTime(7, 0), new LocalTime(12, 0)))
   private val _pmRule = ReceiptPolicyRule("PM rate", 2.5, WeekDays.WorkWeek, LocalTimeInterval(new LocalTime(12, 0), new LocalTime(19, 0)))
+  private val _crossDayRule = ReceiptPolicyRule("Night rate", 0.5, WeekDays.WorkWeek, LocalTimeInterval(new LocalTime(23, 0), new LocalTime(1, 15)))
   private val _carPolicy = ReceiptPolicy(VehicleType.Car, List(_amRule, _pmRule))
 
   private val _service = new TimeMatchingService()
@@ -32,6 +33,11 @@ class TimeMatchingServiceTest extends FlatSpec with Matchers with Assertions {
   "getTotalTimeMatchingPolicyRule" should "ignore weekends" in {
     val period = _service.getTotalTimeMatchingPolicyRule(new DateTime(2015, 9, 19, 6, 0) to new DateTime(2015, 9, 19, 9, 0), _amRule)
     assert(period.millis == 0)
+  }
+
+  "getTotalTimeMatchingPolicyRule" should "correctly match time for a cross day rule" in {
+    val period = _service.getTotalTimeMatchingPolicyRule(new DateTime(2015, 9, 17, 20, 0) to new DateTime(2015, 9, 18, 5, 0), _crossDayRule)
+    assert(period.millis == (2.hours + 15.minutes).millis)
   }
 
 }
